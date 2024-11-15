f9f25fb0cfcbe76845f09e00439c25469f469958
Islam <60185596+IslamMammadov@users.noreply.github.com>
2024-11-15 12:18:46 +0400

    Getting files from email
    This script gets attachments from email with subjects and date filter and then save to a folder. It returns folder path which can be used for data processing.

---
+++ email_files.py
@@ -0,0 +1,39 @@
+import win32com.client
+import os
+from datetime import datetime
+
+def mnp_file_path(subject_keyword, save_folder : str = None):
+    outlook =  win32com.client.Dispatch("Outlook.Application").GetNamespace('MAPI')
+    inbox = outlook.GetDefaultFolder(6) # 6 corresponds to the inbox
+    last_email =  None
+    file_path =None
+    filter_date = datetime.combine(datetime.today(), datetime.min.time()).strftime("%Y-%m-%d %H:%M:%S")
+
+    messages = inbox.Items.Restrict(f"@SQL=""http://schemas.microsoft.com/mapi/proptag/0x0037001f"" LIKE '%s' AND " 
+                                    f"\"urn:schemas:httpmail:datereceived\" > '{filter_date}'" % subject_keyword)
+    for item in messages:
+        try:
+            if last_email is None or item.ReceivedTime > last_email.ReceivedTime:
+                last_email = item
+        except AttributeError as e:
+            print(f'Error>>processing an email: {e}')
+            continue
+
+    if last_email:
+        print(f"Last email details - Sender: {last_email.SenderEmailAddress}, Subject: {last_email.Subject}, Received Time: {last_email.ReceivedTime}")
+        if last_email.Attachments.Count > 0:
+            for attachment in last_email.Attachments:
+                if attachment.FileName.endswith('.xlsx'):
+                    file_path = os.path.join(save_folder, attachment.FileName)
+                    attachment.SaveAsFile(file_path)
+        else:
+            print("No attachments found in the last email")
+    else:
+        print("No matching email found")
+
+    return file_path
+
+
+folder = r'Z:\DWH Load\Islam\src'
+result = mnp_file_path('Daily VAS%',folder)
+print(result)
